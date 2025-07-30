ec77c22240fcf7a8d73867f87894711607e54a09
Kakooza Joel Michael <jkakooza760@gmail.com>
2025-07-30 16:26:30 +0300

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,22 @@
+# calc.py
+x = int(input("Enter the first number: "))
+y= int(input("Enter the second number: "))
+operation = input("Enter the operation (+, -, *, /): ")
+
+if operation == "+":
+    result = x + y
+elif operation == "-":
+    result = x - y
+elif operation == "*":
+    result = x * y
+elif operation == "/":
+
+    if y == 0:
+        result = "Error: Division by zero is undefined."
+    else:
+        result = x / y
+else:
+    result = "Invalid operation."
+
+# Display the result
+print(f"{x} {operation} {y} = {result}")
